92743b09e7d0d1bf2351aa943ad0bf89cd89e29f
Shivam Thakur <shivamth060@gmail.com>
2024-10-16 16:09:40 +0530

    Basic Setup

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Reagime",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("antialiased", poppins.className)}>{children}</body>
+    </html>
+  );
+}
